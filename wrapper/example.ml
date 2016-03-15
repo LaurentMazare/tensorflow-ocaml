5d8dc00ee2bec83c4e5696888a51e8526a1db327
laurent <laurent.mazare@gmail.com>
2016-03-15 21:58:53 +0000

    Add the graph module.

--- wrapper/example.ml
+++ wrapper/example.ml
@@ -1,102 +1,27 @@
 open Wrapper
 module CArray = Ctypes.CArray
 
-let char_list_of_string s =
-  let list = ref [] in
-  for i = String.length s - 1 downto 0 do
-    list := s.[i] :: !list
-  done;
-  !list
-
-let const_1d floats ~name =
-  let open Graph_piqi in
-  let default_attr_value = default_attr_value () in
-  let default_tensor_proto = default_tensor_proto () in
-  { Node_def.name = Some name
-  ; op = Some "Const"
-  ; input = []
-  ; device = None
-  ; attr =
-    [ { Node_def_attr_entry.key = Some "dtype"
-      ; value =
-        Some { default_attr_value with Attr_value.type_ = Some `dt_float }
-      }
-    ; { Node_def_attr_entry.key = Some "value"
-      ; value =
-        Some
-          { default_attr_value with
-            Attr_value.tensor =
-              Some
-                { default_tensor_proto with
-                  Tensor_proto.dtype = Some `dt_float
-                ; float_val = floats
-                ; tensor_shape =
-                  Some
-                    { Tensor_shape_proto.dim =
-                      [ { Tensor_shape_proto_dim.size = Some (List.length floats |> Int64.of_int); name = None } ]
-                    ; unknown_rank = None
-                    }
-                }
-          }
-      }
-    ]
-  }
-
-let binary_op op ~lhs ~rhs ~name =
-  let open Graph_piqi in
-  let default_attr_value = default_attr_value () in
-  { Node_def.name = Some name
-  ; op = Some op
-  ; input = [ lhs; rhs ]
-  ; device = None
-  ; attr =
-    [ { Node_def_attr_entry.key = Some "T"
-      ; value =
-        Some { default_attr_value with Attr_value.type_ = Some `dt_float }
-      }
-    ]
-  }
-
-let generate_protobuf () =
-  let open Graph_piqi in
-  let graph_def =
-    let nodes =
-      [ const_1d [ 4. ] ~name:"Const"
-      ; const_1d [ 2. ] ~name:"Const_1"
-      ; binary_op "Add" ~name:"add" ~lhs:"Const" ~rhs:"Const_1"
-      ]
-    in
-    { Graph_def.node = nodes
-    ; versions =
-      Some
-        { Version_def.producer = Some (Int32.of_int 8)
-        ; min_consumer = None
-        ; bad_consumers = []
-        }
-    ; version = None
-    ; library = None
-    }
-  in
-  let obuf = gen_graph_def graph_def in
-  Piqirun.to_string obuf
-
 let () =
-  let graph_def_str = generate_protobuf () in
+  let graph =
+    Graph.add
+      (Graph.const [ 4. ])
+      (Graph.const [ 38. ])
+  in
   let session_options = Session_options.create () in
   let status = Status.create () in
   let session = Session.create session_options status in
   Printf.printf "%d %s\n%!" (Status.code status) (Status.message status);
   Session.extend_graph
     session
-    graph_def_str
+    (Graph.Protobuf.to_string graph)
     status;
   Printf.printf "%d %s\n%!" (Status.code status) (Status.message status);
   let output_tensors =
     Session.run
       session
       ~inputs:[]
-      ~outputs:[ "add" ]
-      ~targets:[ "add" ]
+      ~outputs:[ Graph.name graph ]
+      ~targets:[ Graph.name graph ]
       status
   in
   Printf.printf "%d %s\n%!" (Status.code status) (Status.message status);
---
+++ wrapper/graph.ml
@@ -0,0 +1,143 @@
+type t =
+  { t_: t_
+  ; name : string
+  }
+and t_ =
+  | Const of float list
+  | Add of t * t
+  | Sub of t * t
+  | Mul of t * t
+  | Div of t * t
+  | Exp of t
+
+let name t = t.name
+
+let fresh_name =
+  let cnt = ref 0 in
+  fun () ->
+    incr cnt;
+    Printf.sprintf "node%d" !cnt
+
+let const floats =
+  { name = fresh_name ()
+  ; t_ = Const floats
+  }
+
+let add lhs rhs =
+  { name = fresh_name ()
+  ; t_ = Add (lhs, rhs)
+  }
+
+let sub lhs rhs =
+  { name = fresh_name ()
+  ; t_ = Add (lhs, rhs)
+  }
+
+let mul lhs rhs =
+  { name = fresh_name ()
+  ; t_ = Mul (lhs, rhs)
+  }
+
+let div lhs rhs =
+  { name = fresh_name ()
+  ; t_ = Div (lhs, rhs)
+  }
+
+let exp arg =
+  { name = fresh_name ()
+  ; t_ = Exp arg
+  }
+
+
+module Protobuf = struct
+  let const_1d floats ~name =
+    let open Graph_piqi in
+    let default_attr_value = default_attr_value () in
+    let default_tensor_proto = default_tensor_proto () in
+    { Node_def.name = Some name
+    ; op = Some "Const"
+    ; input = []
+    ; device = None
+    ; attr =
+      [ { Node_def_attr_entry.key = Some "dtype"
+        ; value =
+          Some { default_attr_value with Attr_value.type_ = Some `dt_float }
+        }
+      ; { Node_def_attr_entry.key = Some "value"
+        ; value =
+          Some
+            { default_attr_value with
+              Attr_value.tensor =
+                Some
+                  { default_tensor_proto with
+                    Tensor_proto.dtype = Some `dt_float
+                  ; float_val = floats
+                  ; tensor_shape =
+                    Some
+                      { Tensor_shape_proto.dim =
+                        [ { Tensor_shape_proto_dim.size = Some (List.length floats |> Int64.of_int); name = None } ]
+                      ; unknown_rank = None
+                      }
+                  }
+            }
+        }
+      ]
+    }
+  
+  let nary op ~input ~name =
+    let open Graph_piqi in
+    let default_attr_value = default_attr_value () in
+    { Node_def.name = Some name
+    ; op = Some op
+    ; input
+    ; device = None
+    ; attr =
+      [ { Node_def_attr_entry.key = Some "T"
+        ; value =
+          Some { default_attr_value with Attr_value.type_ = Some `dt_float }
+        }
+      ]
+    }
+  
+  let binary_op op ~lhs ~rhs ~name =
+    nary op ~input:[ lhs; rhs ] ~name
+  
+  let unary_op op ~arg ~name =
+    nary op ~input:[ arg ] ~name
+
+  let to_string t =
+    let rec walk { name; t_ } =
+      match t_ with
+      | Const floats ->
+        [ const_1d floats ~name ]
+      | Add (lhs, rhs) ->
+        binary_op "Add" ~lhs:lhs.name ~rhs:rhs.name ~name ::
+        (walk lhs @ walk rhs)
+      | Sub (lhs, rhs) ->
+        binary_op "Sub" ~lhs:lhs.name ~rhs:rhs.name ~name ::
+        (walk lhs @ walk rhs)
+      | Mul (lhs, rhs) ->
+        binary_op "Mul" ~lhs:lhs.name ~rhs:rhs.name ~name ::
+        (walk lhs @ walk rhs)
+      | Div (lhs, rhs) ->
+        binary_op "Div" ~lhs:lhs.name ~rhs:rhs.name ~name ::
+        (walk lhs @ walk rhs)
+      | Exp arg ->
+        unary_op "Exp" ~arg:arg.name ~name :: walk arg
+    in
+    let open Graph_piqi in
+    let graph_def =
+      { Graph_def.node = walk t
+      ; versions =
+        Some
+          { Version_def.producer = Some (Int32.of_int 8)
+          ; min_consumer = None
+          ; bad_consumers = []
+          }
+      ; version = None
+      ; library = None
+      }
+    in
+    let obuf = gen_graph_def graph_def in
+    Piqirun.to_string obuf
+end
