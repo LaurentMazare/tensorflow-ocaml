b6fa4c1e09969b3f138ec6d1ea023427ace26783
laurent <laurent.mazare@gmail.com>
2016-03-15 22:06:42 +0000

    Improve example.

--- wrapper/example.ml
+++ wrapper/example.ml
@@ -4,8 +4,8 @@ module CArray = Ctypes.CArray
 let () =
   let graph =
     Graph.add
-      (Graph.const [ 4. ])
-      (Graph.const [ 38. ])
+      (Graph.const [ 4.; 16. ])
+      (Graph.const [ 38.; 16. ])
   in
   let session_options = Session_options.create () in
   let status = Status.create () in
@@ -25,8 +25,8 @@ let () =
   in
   match output with
   | `Ok [ output_tensor ] ->
-    let data = Tensor.data output_tensor Ctypes.float 1 in
-    Printf.printf "%f\n%!" (CArray.get data 0)
+    let data = Tensor.data output_tensor Ctypes.float 2 in
+    Printf.printf "%f %f\n%!" (CArray.get data 0) (CArray.get data 1)
   | `Ok ([] | _ :: _ :: _) -> assert false
   | `Error error -> Printf.printf "Error: %s\n%!" error
 
