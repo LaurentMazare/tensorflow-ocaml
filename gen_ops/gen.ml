42f6744c55500dc9abcf6ad4e0542d583ce3e179
laurent <laurent.mazare@gmail.com>
2016-03-16 20:06:28 +0000

    Use core.

--- gen_ops/gen.ml
+++ gen_ops/gen.ml
@@ -1,3 +1,4 @@
+open Core.Std
 exception Not_supported of string
 
 let value_exn = function
@@ -16,12 +17,13 @@ let read_type types (arg : Op_def_piqi.op_def_arg_def) =
       then "'type'"
       else "'" ^ type_attr
     in
-    if List.mem_assoc type_attr types
-    then
-      List.assoc type_attr types
-      |> String.concat " | "
-      |> fun types -> Printf.sprintf "([< %s ] as %s)" types alpha
-    else alpha
+    begin
+      match List.Assoc.find types type_attr with
+      | None -> alpha
+      | Some types ->
+        String.concat types ~sep:" | "
+        |> fun types -> Printf.sprintf "([< %s ] as %s)" types alpha
+    end
   | None ->
     match arg.type_ with
     | Some `dt_float -> "[ `float ]"
@@ -30,7 +32,7 @@ let read_type types (arg : Op_def_piqi.op_def_arg_def) =
     | None -> raise (Not_supported "no output type")
 
 let extract_types (attrs : Op_def_piqi.op_def_attr_def list) =
-  List.fold_left (fun acc (attr : Op_def_piqi.op_def_attr_def) ->
+  List.filter_map attrs (fun (attr : Op_def_piqi.op_def_attr_def) ->
     match attr.name, attr.type_ with
     | Some name, Some "type" ->
       let allowed_values =
@@ -40,22 +42,16 @@ let extract_types (attrs : Op_def_piqi.op_def_attr_def list) =
           match allowed_values.list with
           | None -> []
           | Some allowed_values ->
-            List.fold_left
-              (fun acc typ ->
-                match typ with
-                | `dt_float -> "`float" :: acc
-                | `dt_double -> "`double" :: acc
-                | _ -> acc
-              )
-              []
-              allowed_values.type_ 
+            List.filter_map allowed_values.type_ (fun typ ->
+              match typ with
+              | `dt_float -> Some "`float"
+              | `dt_double -> Some "`double"
+              | _ -> None)
       in
       if allowed_values = []
-      then acc
-      else (name, allowed_values) :: acc
-    | _ -> acc)
-    []
-    attrs
+      then None
+      else Some (name, allowed_values)
+    | _ -> None)
 
 let gen_mli ops =
   let out_channel = open_out (Printf.sprintf "%s.mli" output_file) in
@@ -69,9 +65,7 @@ let gen_mli ops =
     let name = value_exn op.name in
     try
       let types = extract_types op.attr in
-      let input_types =
-        List.map (read_type types) op.input_arg
-      in
+      let input_types = List.map op.input_arg ~f:(read_type types) in
       let output_type =
         match op.output_arg with
         | [] -> "[ `unit ]"
@@ -80,7 +74,7 @@ let gen_mli ops =
       in
       p "val %s" (String.uncapitalize name);
       p "  :  ?name:string";
-      List.iter (fun typ -> p "  -> %s Node.t" typ) input_types;
+      List.iter input_types ~f:(fun typ -> p "  -> %s Node.t" typ);
       p "  -> %s Node.t" output_type;
       p "";
       Buffer.output_buffer out_channel buffer
@@ -88,7 +82,7 @@ let gen_mli ops =
     | Not_supported str ->
       Printf.printf "Error reading op %s: %s.\n%!" name str
   in
-  List.iter handle_one_op ops;
+  List.iter ops ~f:handle_one_op;
   close_out out_channel
 
 let gen_ml _ops = ()
