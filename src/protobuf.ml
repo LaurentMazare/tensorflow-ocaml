c2882557e562c2aa9dbbd4490fb5747765ab65e6
laurent <laurent.mazare@gmail.com>
2016-03-18 08:04:52 +0000

    Do not process nodes multiple times.

--- src/protobuf.ml
+++ src/protobuf.ml
@@ -8,37 +8,45 @@ open Node
 let of_node t =
   let open Graph_piqi in
   let default_attr_value = default_attr_value () in
+  let nodes = Hashtbl.create 128 in
   let rec walk p =
     let P t = p in
-    let attr =
-      List.map
-        (fun (name, value) ->
-          let value =
-            match value with
-            | Type type_ ->
-              let type_ = Type.to_dt_type type_ in
-              Some { default_attr_value with Attr_value.type_ = Some type_ }
-            (* TODO *)
-            | _ -> None
-          in
-          { Node_def_attr_entry.key = Some name
-          ; value
-          }
-        )
-        t.attributes
-    in
-    let current_node =
-      { Node_def.name = Some (Node.Name.to_string t.name)
-      ; op = Some t.op_name
-      ; input = List.map (fun (P input) -> Node.Name.to_string input.name) t.inputs
-      ; device = None
-      ; attr
-      }
-    in
-    current_node :: List.concat (List.map walk t.inputs)
+    if Hashtbl.mem nodes t.name
+    then ()
+    else begin
+      let attr =
+        List.map
+          (fun (name, value) ->
+            let value =
+              match value with
+              | Type type_ ->
+                let type_ = Type.to_dt_type type_ in
+                Some { default_attr_value with Attr_value.type_ = Some type_ }
+              (* TODO *)
+              | _ -> None
+            in
+            { Node_def_attr_entry.key = Some name
+            ; value
+            }
+          )
+          t.attributes
+      in
+      let node =
+        { Node_def.name = Some (Node.Name.to_string t.name)
+        ; op = Some t.op_name
+        ; input = List.map (fun (P input) -> Node.Name.to_string input.name) t.inputs
+        ; device = None
+        ; attr
+        }
+      in
+      Hashtbl.add nodes t.name node;
+      List.iter walk t.inputs
+    end
   in
+  walk t;
+  let nodes = Hashtbl.fold (fun _ v acc -> v :: acc) nodes [] in
   let graph_def =
-    { Graph_def.node = walk t
+    { Graph_def.node = nodes
     ; versions =
       Some
         { Version_def.producer = Some (Int32.of_int 8)
