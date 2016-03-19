39338f7d57a4b2ad0b3b412bb6ebfc3122ea63d5
laurent <laurent.mazare@gmail.com>
2016-03-19 17:02:14 +0000

    Expose the const int node.

--- src/ops_m.ml
+++ src/ops_m.ml
@@ -1,24 +1,43 @@
 open Node
 
+let get_shape ?shape values =
+  (* TODO: check shape. *)
+  match shape with
+  | Some shape -> shape
+  | None -> [ List.length values ]
+
 let const_float
     ?(name = "Const")
     ?shape
     ~type_
     values
   =
-  (* TODO: check shape, check type_ *)
-  let shape =
-    match shape with
-    | Some shape -> shape
-    | None -> [ List.length values ]
-  in
+  let shape = get_shape ?shape values in
   { name = Name.make_fresh ~name
   ; op_name = "Const"
   ; output_type = type_
-  ; inputs = [  ]
+  ; inputs = []
   ; attributes = [
       "dtype", Type (P type_);
       "value", Tensor_float { type_ = P type_; shape; values };
+    ]
+  ; output_name = None
+  }
+
+let const_int
+    ?(name = "Const")
+    ?shape
+    ~type_
+    values
+  =
+  let shape = get_shape ?shape values in
+  { name = Name.make_fresh ~name
+  ; op_name = "Const"
+  ; output_type = type_
+  ; inputs = []
+  ; attributes = [
+      "dtype", Type (P type_);
+      "value", Tensor_int { type_ = P type_; shape; values };
     ]
   ; output_name = None
   }
--- src/ops_m.mli
+++ src/ops_m.mli
@@ -4,3 +4,10 @@ val const_float
   -> type_:([< `float | `double ] as 'dtype) Node.Type.t
   -> float list
   -> 'dtype Node.t
+
+val const_int
+  :  ?name:string
+  -> ?shape:int list
+  -> type_:([< `int32 | `int64 ] as 'dtype) Node.Type.t
+  -> int list
+  -> 'dtype Node.t
