979f7746fb2465c835271eb8d20f13348271beac
laurent <laurent.mazare@gmail.com>
2016-03-19 16:58:20 +0000

    Better api type.

--- src/ops_m.mli
+++ src/ops_m.mli
@@ -1,6 +1,6 @@
 val const_float
   :  ?name:string
   -> ?shape:int list
-  -> type_:'dtype Node.Type.t
+  -> type_:([< `float | `double ] as 'dtype) Node.Type.t
   -> float list
   -> 'dtype Node.t
