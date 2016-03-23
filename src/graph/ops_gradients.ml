994a52628c2a238c8d5798c08b212e230236c70a
laurent <laurent.mazare@gmail.com>
2016-03-23 06:12:51 +0000

    Add a helper for unary functions.

--- src/graph/ops_gradients.ml
+++ src/graph/ops_gradients.ml
@@ -1,6 +1,27 @@
 open Core.Std
 
+type unary =
+  { f1 : 'a .
+          (  [< `float | `double] as 'a) Node.t
+          -> gradient:'a Node.t
+          -> 'a Node.t
+  }
+
 let all = List.map ~f:Option.some
+
+let unary_exn (type a) ~self ~(gradient : a Node.t) ~t =
+  let Node.P input =
+    match self.Node.inputs with
+    | [] | _ :: _ :: _ -> failwith "Not a unary function"
+    | [ node ] -> node
+  in
+  let output =
+    match input.output_type, gradient.Node.output_type with
+    | Node.Type.Double, Node.Type.Double -> Node.P (t.f1 input ~gradient)
+    | Node.Type.Float, Node.Type.Float -> Node.P (t.f1 input ~gradient)
+    | _ -> failwith "Inconsistent types"
+  in
+  all [ output ]
 
 let add_gradient ~self:_ ~gradient =
   let gradient = Node.P gradient in
@@ -12,14 +33,8 @@ let sub_gradient ~self:_ ~gradient =
   all [ gradient; minus_gradient ]
 
 let abs_gradient (type a) ~self ~(gradient : a Node.t) =
-  let Node.P input = List.hd_exn self.Node.inputs in
-  let output =
-    match input.output_type, gradient.Node.output_type with
-    | Node.Type.Double, Node.Type.Double -> Node.P (Ops.mul gradient (Ops.sign input))
-    | Node.Type.Float, Node.Type.Float -> Node.P (Ops.mul gradient (Ops.sign input))
-    | _ -> assert false
-  in
-  all [ output ]
+  let t = { f1 = fun input ~gradient -> Ops.mul gradient (Ops.sign input) } in
+  unary_exn ~self ~gradient ~t
 
 let register_all () =
   Gradients.register_gradient (Node.Op_name.of_string "Add") { f = add_gradient };
