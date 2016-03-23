0b5e25092c73d09f60fdfaf0b7b6769f33c076cc
laurent <laurent.mazare@gmail.com>
2016-03-23 05:53:55 +0000

    Add abs.

--- src/graph/ops_gradients.ml
+++ src/graph/ops_gradients.ml
@@ -11,7 +11,18 @@ let sub_gradient ~self:_ ~gradient =
   let gradient = Node.P gradient in
   all [ gradient; minus_gradient ]
 
+let abs_gradient (type a) ~self ~(gradient : a Node.t) =
+  let Node.P input = List.hd_exn self.Node.inputs in
+  let output =
+    match input.output_type, gradient.Node.output_type with
+    | Node.Type.Double, Node.Type.Double -> Node.P (Ops.mul gradient (Ops.sign input))
+    | Node.Type.Float, Node.Type.Float -> Node.P (Ops.mul gradient (Ops.sign input))
+    | _ -> assert false
+  in
+  all [ output ]
+
 let register_all () =
   Gradients.register_gradient (Node.Op_name.of_string "Add") { f = add_gradient };
-  Gradients.register_gradient (Node.Op_name.of_string "Sub") { f = sub_gradient }
+  Gradients.register_gradient (Node.Op_name.of_string "Sub") { f = sub_gradient };
+  Gradients.register_gradient (Node.Op_name.of_string "Abs") { f = abs_gradient }
 
