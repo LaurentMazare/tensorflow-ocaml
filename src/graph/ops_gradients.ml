eadf866b6e5624ce8b361b41fdb302b090f69cc3
laurent <laurent.mazare@gmail.com>
2016-03-23 17:31:24 +0000

    Add matmul gradient.

--- src/graph/ops_gradients.ml
+++ src/graph/ops_gradients.ml
@@ -1,4 +1,6 @@
 open Core.Std
+module N = Node
+module T = Node.Type
 
 type unary =
   { f1 : 'a .
@@ -24,6 +26,17 @@ let pointwise_unary_exn (type a) ~self ~(gradient : a Node.t) ~t =
   in
   all [ output ]
 
+let binary_extract_exn : type a . a Node.t -> (a Node.t * a Node.t) = fun node ->
+  let Node.P lhs, Node.P rhs =
+    match node.inputs with
+    | [ lhs; rhs ] -> lhs, rhs
+    | _ -> failwith "Not a binary function"
+  in
+  match lhs.output_type, rhs.output_type, node.output_type with
+  | T.Double, T.Double, T.Double -> lhs, rhs
+  | T.Float, T.Float, T.Float -> lhs, rhs
+  | _ -> failwith "Inconsistent types"
+
 let add_gradient ~self:_ ~gradient =
   let gradient = Node.P gradient in
   all [ gradient; gradient ]
@@ -43,9 +56,42 @@ let square_gradient (type a) ~self ~(gradient : a Node.t) =
   in
   pointwise_unary_exn ~self ~gradient ~t
 
-let register_all () =
-  Gradients.register_gradient (Node.Op_name.of_string "Add") { f = add_gradient };
-  Gradients.register_gradient (Node.Op_name.of_string "Sub") { f = sub_gradient };
-  Gradients.register_gradient (Node.Op_name.of_string "Abs") { f = abs_gradient };
-  Gradients.register_gradient (Node.Op_name.of_string "Square") { f = square_gradient }
+let matmul_gradient ~self ~gradient =
+  let get_transpose str =
+    List.Assoc.find self.Node.attributes str
+    |> Option.value_map
+        ~default:false
+        ~f:(function
+          | Node.Bool b -> b
+          | _ -> assert false)
+  in
+  let transpose_a = get_transpose "transpose_a" in
+  let transpose_b = get_transpose "transpose_b" in
+  let lhs, rhs = binary_extract_exn self in
+  match transpose_a, transpose_b with
+  | false, false ->
+    [ N.P (Ops.matMul gradient rhs ~transpose_b:true)
+    ; N.P (Ops.matMul lhs gradient ~transpose_a:true)
+    ] |> all
+  | false, true ->
+    [ N.P (Ops.matMul gradient rhs)
+    ; N.P (Ops.matMul gradient lhs ~transpose_a:true)
+    ] |> all
+  | true, false ->
+    [ N.P (Ops.matMul rhs gradient ~transpose_b:true)
+    ; N.P (Ops.matMul lhs gradient)
+    ] |> all
+  | true, true ->
+    [ N.P (Ops.matMul rhs gradient ~transpose_a:true ~transpose_b:true)
+    ; N.P (Ops.matMul gradient lhs ~transpose_a:true ~transpose_b:true)
+    ] |> all
 
+let register_all () =
+  List.iter ~f:(fun (name, f) ->
+    Gradients.register_gradient (Node.Op_name.of_string name) f)
+    [ "Add", { Gradients.f = add_gradient }
+    ; "Sub", { f = sub_gradient }
+    ; "Abs", { f = abs_gradient }
+    ; "Square", { f = square_gradient }
+    ; "MatMul", { f = matmul_gradient }
+    ]
