5580a0c00c48829f3c8a37fc470a069e64d1ff59
laurent <laurent.mazare@gmail.com>
2016-03-23 21:12:35 +0000

    Factorize some of the example code.

---
+++ examples/helper.ml
@@ -0,0 +1,50 @@
+open Core.Std
+open Wrapper
+module CArray = Ctypes.CArray
+
+(* TODO: move the interesting functions to src/. *)
+let ok_exn (result : 'a Session.result) ~context =
+  match result with
+  | Ok result -> result
+  | Error status ->
+    Printf.sprintf "Error in %s: %s" context (Status.message status)
+    |> failwith
+
+let const_float shape f =
+  let size = List.fold shape ~init:1 ~f:(( * )) in
+  Ops_m.const_float
+    ~type_:Float
+    ~shape
+    (List.init size ~f:(const f))
+
+let print_one_tensor (name, tensor) =
+  Printf.printf "%s:\n%!" name;
+  let dim = Tensor.dim tensor 0 in
+  let data = Tensor.data tensor Ctypes.float dim in
+  for d = 0 to dim - 1 do
+    Printf.printf "%d %f\n%!" d (CArray.get data d)
+  done
+
+let print_tensors tensors ~names =
+  List.zip_exn names tensors
+  |> List.iter ~f:print_one_tensor
+
+let run session ~inputs ~outputs ~targets =
+  let f n = Node.Name.to_string n.Node.name in
+  Session.run
+    session
+    ~inputs:(List.map inputs ~f:(fun (name, tensor) -> f name, tensor))
+    ~outputs:(List.map outputs ~f)
+    ~targets:(List.map targets ~f)
+  |> ok_exn ~context:"session_run"
+
+let create_session nodes =
+  let session =
+    Session.create ()
+    |> ok_exn ~context:"session creation"
+  in
+  Session.extend_graph
+    session
+    (Node_protobuf.of_nodes nodes)
+  |> ok_exn ~context:"extending graph";
+  session
