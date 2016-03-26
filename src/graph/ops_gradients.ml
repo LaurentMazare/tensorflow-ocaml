a238d3b35f3ed1cb46733f7fcc69bf5f763ef91c
laurent <laurent.mazare@gmail.com>
2016-03-26 21:19:16 +0000

    And again add more gradients.

--- src/graph/ops_gradients.ml
+++ src/graph/ops_gradients.ml
@@ -3,7 +3,8 @@ module N = Node
 module T = Node.Type
 
 type unary =
-  { f1 : 'a .input:(  [< `float | `double] as 'a) N.t
+  { f1 : 'a .x:(  [< `float | `double] as 'a) N.t
+          -> y:'a N.t
           -> gradient:'a N.t
           -> 'a N.t
   }
@@ -11,17 +12,17 @@ type unary =
 let all = List.map ~f:Option.some
 
 let pointwise_unary_exn (type a) ~self ~(gradient : a N.t) ~t =
-  let N.P input =
+  let N.P x =
     match self.N.inputs with
     | [] | _ :: _ :: _ -> failwith "Not a unary function"
     | [ node ] -> node
   in
   let output =
-    match input.output_type, gradient.N.output_type with
+    match x.output_type, gradient.N.output_type with
     | T.Double, T.Double ->
-      N.P (t.f1 ~input ~gradient)
+      N.P (t.f1 ~x ~y:self ~gradient)
     | T.Float, T.Float ->
-      N.P (t.f1 ~input ~gradient)
+      N.P (t.f1 ~x ~y:self ~gradient)
     | _ -> failwith "Inconsistent types"
   in
   all [ output ]
@@ -76,25 +77,38 @@ let div_gradient ~self ~gradient =
   in
   all [ N.P lhs_gradient; N.P rhs_gradient ]
 
+let pow_gradient ~self ~gradient =
+  let lhs, rhs = binary_extract_exn self in
+  let shape_lhs = Ops.shape lhs in
+  let shape_rhs = Ops.shape rhs in
+  let rlhs, rrhs = Ops_m.broadcast_gradient_args shape_lhs shape_rhs in
+  let one = Ops_m.const_float ~type_:self.N.output_type [ 1. ] in
+  let lhs_gradient =
+    Ops.reshape (Ops.sum Ops_m.(gradient * rhs * Ops.pow lhs (rhs - one)) rlhs) shape_lhs
+  in
+  let rhs_gradient =
+    Ops.reshape (Ops.sum Ops_m.(gradient * self * Ops.log lhs) rrhs) shape_rhs
+  in
+  all [ N.P lhs_gradient; N.P rhs_gradient ]
 
 let neg_gradient ~self:_ ~gradient =
   all [ N.P (Ops.neg gradient) ]
 
 let abs_gradient (type a) ~self ~(gradient : a N.t) =
-  let t = { f1 = fun ~input ~gradient -> Ops.sign input |> Ops.mul gradient } in
+  let t = { f1 = fun ~x ~y:_ ~gradient -> Ops.sign x |> Ops.mul gradient } in
   pointwise_unary_exn ~self ~gradient ~t
 
 let square_gradient (type a) ~self ~(gradient : a N.t) =
   let t =
-    { f1 = fun ~input ~gradient ->
-        Ops.mul input (Ops_m.scalar ~type_:input.output_type 2.)
+    { f1 = fun ~x ~y:_ ~gradient ->
+        Ops.mul x (Ops_m.scalar ~type_:x.output_type 2.)
         |> Ops.mul gradient
     }
   in
   pointwise_unary_exn ~self ~gradient ~t
 
 let log_gradient (type a) ~self ~(gradient : a N.t) =
-  let t = { f1 = fun ~input ~gradient -> Ops.mul gradient (Ops.inv input) } in
+  let t = { f1 = fun ~x ~y:_ ~gradient -> Ops.mul gradient (Ops.inv x) } in
   pointwise_unary_exn ~self ~gradient ~t
 
 let relu_gradient ~self ~gradient =
@@ -216,15 +230,52 @@ let sign_gradient ~self ~gradient:_ =
   all [ N.P (Ops.zerosLike self) ]
 
 let sin_gradient (type a) ~self ~(gradient : a N.t) =
-  let t = { f1 = fun ~input ~gradient -> Ops.mul gradient (Ops.cos input) } in
+  let t = { f1 = fun ~x ~y:_ ~gradient -> Ops.mul gradient (Ops.cos x) } in
   pointwise_unary_exn ~self ~gradient ~t
 
 let cos_gradient (type a) ~self ~(gradient : a N.t) =
-  let t = { f1 = fun ~input ~gradient -> Ops.mul gradient (Ops.sin input) |> Ops.neg } in
+  let t = { f1 = fun ~x ~y:_ ~gradient -> Ops.mul gradient (Ops.sin x) |> Ops.neg } in
   pointwise_unary_exn ~self ~gradient ~t
 
 let addn_gradient ~self ~gradient =
   List.map self.N.inputs ~f:(fun _ -> Some (N.P gradient))
+
+let inv_gradient ~self ~gradient =
+  [ Some (N.P (Ops.mul gradient (Ops.neg (Ops.square self)))) ]
+
+let rsqrt_gradient (type a) ~self ~(gradient : a N.t) =
+  let t =
+    { f1 = fun ~x ~y ~gradient ->
+        Ops_m.(gradient * const_float ~type_:y.N.output_type [ -0.5 ] * Ops.inv x * y)
+    }
+  in
+  pointwise_unary_exn ~self ~gradient ~t
+
+let two_over_pi = 2. /. 3.1415926535897932384626434
+
+let erf_gradient (type a) ~self ~(gradient : a N.t) =
+  let t =
+    { f1 = fun ~x ~y ~gradient ->
+        Ops_m.(gradient * const_float ~type_:y.N.output_type [ two_over_pi ]
+          * Ops.exp (Ops.neg (Ops.square x)))
+    }
+  in
+  pointwise_unary_exn ~self ~gradient ~t
+
+let erfc_gradient (type a) ~self ~(gradient : a N.t) =
+  let t =
+    { f1 = fun ~x ~y ~gradient ->
+        Ops_m.(gradient * const_float ~type_:y.N.output_type [ -. two_over_pi ]
+          * Ops.exp (Ops.neg (Ops.square x)))
+    }
+  in
+  pointwise_unary_exn ~self ~gradient ~t
+
+let lgamma_gradient (type a) ~self ~(gradient : a N.t) =
+  let t =
+    { f1 = fun ~x ~y:_ ~gradient -> Ops.mul gradient (Ops.digamma x) }
+  in
+  pointwise_unary_exn ~self ~gradient ~t
 
 let register_all () =
   let module O = Ops.Op_names in
@@ -234,7 +285,11 @@ let register_all () =
     ; O.addN,    { f = addn_gradient }
     ; O.cos,     { f = cos_gradient }
     ; O.div,     { f = div_gradient }
+    ; O.erf,     { f = erf_gradient }
+    ; O.erfc,    { f = erfc_gradient }
     ; O.exp,     { f = exp_gradient }
+    ; O.inv,     { f = inv_gradient }
+    ; O.lgamma,  { f = lgamma_gradient }
     ; O.log,     { f = log_gradient }
     ; O.matMul,  { f = matmul_gradient }
     ; O.max,     { f = minmax_gradient }
@@ -242,7 +297,9 @@ let register_all () =
     ; O.min,     { f = minmax_gradient }
     ; O.mul,     { f = mul_gradient }
     ; O.neg,     { f = neg_gradient }
+    ; O.pow,     { f = pow_gradient }
     ; O.relu,    { f = relu_gradient }
+    ; O.rsqrt,   { f = rsqrt_gradient }
     ; O.sigmoid, { f = sigmoid_gradient }
     ; O.sign,    { f = sign_gradient }
     ; O.sin,     { f = sin_gradient }
