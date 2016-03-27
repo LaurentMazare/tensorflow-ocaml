23ae93562a5376bf750a7753009c5b16781edfe1
laurent <laurent.mazare@gmail.com>
2016-03-27 23:04:10 +0100

    Adapt more examples to the new session.

--- examples/basics/linear_regression_common.ml
+++ examples/basics/linear_regression_common.ml
@@ -6,42 +6,22 @@ let run ~samples ~size_xs ~size_ys ~xs ~ys =
   let ys = List.concat ys in
   let xs = Ops_m.cf ~shape:[samples; size_xs] xs in
   let y  = Ops_m.cf ~shape:[samples; size_ys] ys in
-  let w = Ops_m.varf [ size_xs; size_ys ] in
-  let b = Ops_m.varf [ size_ys ] in
-  let w_assign = Ops.assign w (Ops_m.f ~shape:[ size_xs; size_ys ] 0.) in
-  let b_assign = Ops.assign b (Ops_m.f ~shape:[ size_ys ] 0.) in
+  let w = Var.f [ size_xs; size_ys ] 0. in
+  let b = Var.f [ size_ys ] 0. in
   let y_ = Ops_m.(xs *^ w + b) in
   let err = Ops_m.(Ops.square (y_ - y) |> reduce_mean) in
   let gd =
     Optimizers.gradient_descent_minimizer ~alpha:0.04 ~varsf:[ w; b ] err
   in
-  let session =
-    H.create_session (Node.[ P err; P w_assign; P b_assign ] @ gd)
-  in
-  let _output =
-    H.run session
-      ~outputs:[]
-      ~targets:[ w_assign; b_assign ] 
-  in
+  let session = Session.create () in
   let results = ref [] in
   let print_err n =
-    let output =
-      H.run session
-        ~outputs:[ err; y_ ]
-        ~targets:[ err ]
-    in
-    match output with
-    | [ err; y_ ] ->
-      H.print_tensors [ err ] ~names:[ sprintf "err %d" n ];
-      results := (n, Tensor.to_float_list y_) :: !results
-    | _ -> assert false
+    let err, y_ = Session.run session Session.Output.(both (float err) (float y_)) in
+    H.print_tensors [ Tensor.P err ] ~names:[ sprintf "err %d" n ];
+    results := (n, Tensor.to_float_list (Tensor.P y_)) :: !results
   in
   for i = 0 to 2000 do
-    let output =
-      Wrapper.Session.run session
-        ~targets:(List.map gd ~f:(fun n -> Node.packed_name n |> Node.Name.to_string))
-    in
-    ignore output;
+    Session.run session Session.Output.empty ~targets:gd;
     if i % 400 = 0 then print_err i
   done;
   !results
