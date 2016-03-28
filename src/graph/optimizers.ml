68c8417ff4a9ef77a4d400d3465c7af177b52ed2
laurent <laurent.mazare@gmail.com>
2016-03-28 18:17:50 +0100

    Add the adam optimizer.

--- src/graph/optimizers.ml
+++ src/graph/optimizers.ml
@@ -63,3 +63,64 @@ let momentum_minimizer ~alpha ~momentum ?(varsf = []) ?(varsd = []) target =
     else []
   in
   gdf @ gdd
+
+let adam_minimizer ~alpha ?beta1 ?beta2 ?epsilon ?(varsf = []) ?(varsd = []) target =
+  let beta1 =
+    match beta1 with
+    | None -> Ops.f 0.9
+    | Some v -> v
+  in
+  let beta2 =
+    match beta2 with
+    | None -> Ops.f 0.999
+    | Some v -> v
+  in
+  let epsilon =
+    match epsilon with
+    | None -> Ops.f 1e-8
+    | Some v -> v
+  in
+  let gradsf, gradsd =
+    Gradients.gradient target
+      ~with_respect_to_float:varsf
+      ~with_respect_to_double:varsd
+  in
+  let apply_adam grads vars ~alpha ~beta1 ~beta2 ~epsilon =
+    List.map2_exn grads vars ~f:(fun grad var ->
+      let var_shape = Ops.shape var in
+      let create_var () =
+        Var.create (get_shape var) ~type_:var.output_type
+          ~init:(Ops.fill var_shape (Ops.scalar ~empty_shape:() ~type_:var.output_type 0.))
+      in
+      let grad = Ops.reshape grad var_shape in
+      let adam =
+        Ops.applyAdam
+          var
+          (create_var ()) (* m *)
+          (create_var ()) (* v *)
+          (create_var ()) (* beta1_power *)
+          (create_var ()) (* beta2_power *)
+          alpha
+          beta1
+          beta2
+          epsilon
+          grad
+      in
+      Node.P adam)
+  in
+  let gdf =
+    if not (List.is_empty varsf)
+    then apply_adam gradsf varsf ~alpha ~beta1 ~beta2 ~epsilon
+    else []
+  in
+  let gdd =
+    if not (List.is_empty varsd)
+    then
+      apply_adam gradsd varsd
+        ~alpha:(Ops.cast alpha ~type_:Double)
+        ~beta1:(Ops.cast beta1 ~type_:Double)
+        ~beta2:(Ops.cast beta2 ~type_:Double)
+        ~epsilon:(Ops.cast epsilon ~type_:Double)
+    else []
+  in
+  gdf @ gdd
--- src/graph/optimizers.mli
+++ src/graph/optimizers.mli
@@ -13,3 +13,13 @@ val momentum_minimizer
   -> ?varsd : [ `double ] Node.t list (* Have to be variables. *)
   -> [< `float | `double ] Node.t
   -> Node.p list
+
+val adam_minimizer
+  :  alpha : [ `float ] Node.t
+  -> ?beta1 : [ `float ] Node.t
+  -> ?beta2 : [ `float ] Node.t
+  -> ?epsilon : [ `float ] Node.t
+  -> ?varsf : [ `float ] Node.t list (* Have to be variables. *)
+  -> ?varsd : [ `double ] Node.t list (* Have to be variables. *)
+  -> [< `float | `double ] Node.t
+  -> Node.p list
