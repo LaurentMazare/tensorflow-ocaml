e6b51be8d3d02539e515fc89cd6ad3eca13c9e59
laurent <laurent.mazare@gmail.com>
2016-04-23 16:23:06 +0100

    Cleanup the char rnn code.

--- examples/char_rnn/char_rnn.ml
+++ examples/char_rnn/char_rnn.ml
@@ -12,61 +12,98 @@ let gen_len = 500
 let sample_size = 25
 let temperature = 0.5
 
-let lstm ~size_c ~size_x ~size_y ~prev_mem x_and_ys =
-  let wy, by = Var.normalf [ size_c; size_y ] ~stddev:0.1, Var.f [ size_y ] 0. in
-  let lstm1 = Staged.unstage (Cell.lstm ~size_c ~size_x) in
+type t =
+  { train_err              : [ `float ] Node.t
+  ; train_output_mem       : [ `float ] Node.t
+  ; train_placeholder_mem  : [ `float ] Node.t
+  ; train_placeholder_x    : [ `float ] Node.t
+  ; train_placeholder_y    : [ `float ] Node.t
+  ; sample_output          : [ `float ] Node.t
+  ; sample_output_mem      : [ `float ] Node.t
+  ; sample_placeholder_mem : [ `float ] Node.t
+  ; sample_placeholder_x   : [ `float ] Node.t
+  }
+
+let rnn ~size_c ~sample_size ~alphabet_size =
+  let train_placeholder_mem  = Ops.placeholder ~type_:Float [] in
+  let train_placeholder_x    = Ops.placeholder ~type_:Float [] in
+  let train_placeholder_y    = Ops.placeholder ~type_:Float [] in
+  let sample_placeholder_mem = Ops.placeholder ~type_:Float [] in
+  let sample_placeholder_x   = Ops.placeholder ~type_:Float [] in
+  (* Two LSTM specific code. *)
+  let wy, by =
+    Var.normalf [ size_c; alphabet_size ] ~stddev:0.1, Var.f [ alphabet_size ] 0.
+  in
+  let lstm1 = Staged.unstage (Cell.lstm ~size_c ~size_x:alphabet_size) in
   let lstm2 = Staged.unstage (Cell.lstm ~size_c ~size_x:size_c) in
-  let two_lstm ~mem ~x =
-    let h1, c1, h2, c2 = mem in
+  let two_lstm ~mem:(h1, c1, h2, c2) ~x =
     let `h h1, `c c1 = lstm1 ~h:h1 ~c:c1 ~x in
     let `h h2, `c c2 = lstm2 ~h:h2 ~c:c2 ~x:h1 in
     let y_bar = Ops.(h2 *^ wy + by) |> Ops.softmax in
     y_bar, (h1, c1, h2, c2)
   in
-  List.fold x_and_ys ~init:([], prev_mem) ~f:(fun (errs, mem) (x, y) ->
-    let y_bar, mem = two_lstm ~mem ~x in
-    let err = Ops.(neg (y * log y_bar)) in
-    err :: errs, mem)
-  |> fun (errs, mem) ->
-  match errs with
-  | [] -> failwith "Empty input list"
-  | [ err ] -> err, mem, two_lstm
-  | errs ->
-    let err = Ops.concat Ops.one32 errs |> Ops.reduce_sum in
-    err, mem, two_lstm
+  let split node =
+    Ops.split Ops.zero32 node ~num_split:sample_size
+    |> List.map ~f:(fun n ->
+      Ops.reshape n (Ops.const_int ~type_:Int32 [ 1; alphabet_size ]))
+  in
+  let x_and_ys =
+    List.zip_exn (split train_placeholder_x) (split train_placeholder_y)
+  in
+  let mem_split mem =
+    match Ops.split Ops.one32 mem ~num_split:4 with
+    | [ h1; c1; h2; c2 ] -> h1, c1, h2, c2
+    | _ -> assert false
+  in
+  let train_err, train_output_mem =
+    List.fold x_and_ys
+      ~init:([], mem_split train_placeholder_mem)
+      ~f:(fun (errs, mem) (x, y) ->
+        let y_bar, mem = two_lstm ~x ~mem in
+        let err = Ops.(neg (y * log y_bar)) in
+        err :: errs, mem)
+  in
+  let train_err =
+    match train_err with
+    | [] -> failwith "sample_size is 0"
+    | [ err ] -> err
+    | errs -> Ops.concat Ops.one32 errs |> Ops.reduce_sum
+  in
+  let mem_concat (h1, c1, h2, c2) = Ops.concat Ops.one32 [ h1; c1; h2; c2 ] in
+  let sample_output, sample_output_mem =
+    two_lstm ~mem:(mem_split sample_placeholder_mem) ~x:sample_placeholder_x
+  in
+  { train_err
+  ; train_output_mem = mem_concat train_output_mem
+  ; train_placeholder_mem
+  ; train_placeholder_x
+  ; train_placeholder_y
+  ; sample_output
+  ; sample_output_mem = mem_concat sample_output_mem
+  ; sample_placeholder_mem
+  ; sample_placeholder_x
+  }
 
 let tensor_zero size =
   let tensor = Tensor.create2 Float32 1 size in
   Tensor.fill tensor 0.;
   tensor
 
-let print_sample two_lstm all_chars =
+let print_sample t all_chars =
   let alphabet_size = Array.length all_chars in
-  let placeholder_h1 = Ops.placeholder ~type_:Float [] in
-  let placeholder_c1 = Ops.placeholder ~type_:Float [] in
-  let placeholder_h2 = Ops.placeholder ~type_:Float [] in
-  let placeholder_c2 = Ops.placeholder ~type_:Float [] in
-  let prev_mem = placeholder_h1, placeholder_c1, placeholder_h2, placeholder_c2 in
-  let x = Ops.placeholder [] ~type_:Float in
-  let y_bar, sample_mem = two_lstm ~mem:prev_mem ~x in
   Staged.stage (fun ~prev_y ~prev_mem_data ->
     let init = [], prev_y, prev_mem_data in
     let ys, _, _ =
       List.fold (List.range 0 gen_len) ~init ~f:(fun (acc_y, prev_y, prev_mem_data) _ ->
         let inputs =
-          let prev_h1, prev_c1, prev_h2, prev_c2 = prev_mem_data in
           Session.Input.
-            [ float x prev_y
-            ; float placeholder_h1 prev_h1
-            ; float placeholder_c1 prev_c1
-            ; float placeholder_h2 prev_h2
-            ; float placeholder_c2 prev_c2
+            [ float t.sample_placeholder_x prev_y
+            ; float t.sample_placeholder_mem prev_mem_data
             ]
         in
-        let h1_out, c1_out, h2_out, c2_out = sample_mem in
-        let y_res, h1_res, c1_res, h2_res, c2_res =
+        let y_res, mem_data =
           Session.run ~inputs
-            Session.Output.(five (float y_bar) (float h1_out) (float c1_out) (float h2_out) (float c2_out))
+            Session.Output.(both (float t.sample_output) (float t.sample_output_mem))
         in
         let dist =
           Array.init alphabet_size ~f:(fun i ->
@@ -81,7 +118,7 @@ let print_sample two_lstm all_chars =
         done;
         let y_res = tensor_zero alphabet_size in
         Tensor.set y_res [| 0; !y |] 1.;
-        !y :: acc_y, y_res, (h1_res, c1_res, h2_res, c2_res))
+        !y :: acc_y, y_res, mem_data)
     in
     List.rev ys
     |> List.map ~f:(fun i -> all_chars.(i))
@@ -91,53 +128,29 @@ let print_sample two_lstm all_chars =
 let fit_and_evaluate data all_chars =
   let alphabet_size = Array.length all_chars in
   let input_size = (Tensor.dims data).(0) in
-  let placeholder_x = Ops.placeholder ~type_:Float [] in
-  let placeholder_y = Ops.placeholder ~type_:Float [] in
-  let split node =
-    Ops.split Ops.zero32 node ~num_split:sample_size
-    |> List.map ~f:(fun n ->
-      Ops.reshape n (Ops.const_int ~type_:Int32 [ 1; alphabet_size ]))
-  in
-  let xs = split placeholder_x in
-  let ys = split placeholder_y in
-  let x_and_ys = List.zip_exn xs ys in
-  let placeholder_h1 = Ops.placeholder ~type_:Float [] in
-  let placeholder_c1 = Ops.placeholder ~type_:Float [] in
-  let placeholder_h2 = Ops.placeholder ~type_:Float [] in
-  let placeholder_c2 = Ops.placeholder ~type_:Float [] in
-  let prev_mem = placeholder_h1, placeholder_c1, placeholder_h2, placeholder_c2 in
-  let err, train_mem, two_lstm =
-    lstm ~size_c ~size_x:alphabet_size ~size_y:alphabet_size ~prev_mem x_and_ys
-  in
-  let gd = Optimizers.adam_minimizer err ~learning_rate:(Ops.f 0.004) in
-  let print_sample = Staged.unstage (print_sample two_lstm all_chars) in
-  let zero = tensor_zero size_c in
+  let t = rnn ~size_c ~sample_size ~alphabet_size in
+  let gd = Optimizers.adam_minimizer t.train_err ~learning_rate:(Ops.f 0.004) in
+  let print_sample = Staged.unstage (print_sample t all_chars) in
+  let zero = tensor_zero (4 * size_c) in
   List.fold (List.range 1 epochs)
-    ~init:(log (float alphabet_size) *. float sample_size, (zero, zero, zero, zero))
+    ~init:(log (float alphabet_size) *. float sample_size, zero)
     ~f:(fun (smooth_error, prev_mem_data) i ->
       let start_idx = (i * sample_size) % (input_size - sample_size - 1) in
       let x_data = Tensor.sub_left data start_idx sample_size in
       let y_data = Tensor.sub_left data (start_idx + 1) sample_size in
       let inputs =
-        let prev_h1, prev_c1, prev_h2, prev_c2 = prev_mem_data in
         Session.Input.
-          [ float placeholder_x  x_data
-          ; float placeholder_y  y_data
-          ; float placeholder_h1 prev_h1
-          ; float placeholder_c1 prev_c1
-          ; float placeholder_h2 prev_h2
-          ; float placeholder_c2 prev_c2
+          [ float t.train_placeholder_x x_data
+          ; float t.train_placeholder_y y_data
+          ; float t.train_placeholder_mem prev_mem_data
           ]
       in
-      let h1_out, c1_out, h2_out, c2_out = train_mem in
-      let err, h1_res, c1_res, h2_res, c2_res =
+      let err, mem_data =
         Session.run
           ~inputs
           ~targets:gd
-          Session.Output.(five
-            (scalar_float err) (float h1_out) (float c1_out) (float h2_out) (float c2_out))
+          Session.Output.(both (scalar_float t.train_err) (float t.train_output_mem))
       in
-      let mem_data = h1_res, c1_res, h2_res, c2_res in
       let smooth_error = 0.999 *. smooth_error +. 0.001 *. err in
       if i % 500 = 0 then begin
         printf "Epoch: %d %f\n%!" i smooth_error;
