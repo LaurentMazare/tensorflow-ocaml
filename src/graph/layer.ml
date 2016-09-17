c6c1143a756214d75ba1ed7a18a439c56025b904
laurent <laurent.mazare@gmail.com>
2016-09-17 12:28:23 +0100

    Tweak the batch normalization.

--- src/graph/layer.ml
+++ src/graph/layer.ml
@@ -2,9 +2,9 @@ open Core_kernel.Std
 
 let batch_normalization ?(decay = 0.99) t ~testing ~dims ~feature_count =
   let type_ = Node.output_type t in
-  let zero = Ops.const_float ~type_ [ 0. ] in
-  let one = Ops.const_float ~type_ [ 1. ] in
-  let one_minus_decay = Ops.const_float ~type_ [ 1. -. decay ] in
+  let zero = Ops.const_float ~type_ (List.init feature_count ~f:(fun _ -> 0.)) in
+  let one = Ops.const_float ~type_ (List.init feature_count ~f:(fun _ -> 1.)) in
+  let one_minus_decay = Ops.scalar ~type_ (1. -. decay) in
   let beta = Var.create [ feature_count ] ~type_ ~init:zero in
   let gamma = Var.create [ feature_count ] ~type_ ~init:one in
   let batch_moments = Ops.moments t ~dims:(List.init dims ~f:Fn.id) in
@@ -15,7 +15,7 @@ let batch_normalization ?(decay = 0.99) t ~testing ~dims ~feature_count =
         beta
         Ops.(one_minus_decay * (beta - batch_moments.mean))
     in
-    Ops.select
+    Ops.select ~name:"select-beta"
       testing
       beta
       (Ops.identity ~control_inputs:[ Node.P update_beta ] beta)
@@ -27,7 +27,7 @@ let batch_normalization ?(decay = 0.99) t ~testing ~dims ~feature_count =
         gamma
         Ops.(one_minus_decay * (gamma - batch_moments.variance))
     in
-    Ops.select
+    Ops.select ~name:"select-gamma"
       testing
       gamma
       (Ops.identity ~control_inputs:[ Node.P update_gamma ] gamma)
