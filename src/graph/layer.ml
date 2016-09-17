94a354c4e1d29237d103c2a4d0d06b52da922bc0
laurent <laurent.mazare@gmail.com>
2016-09-17 11:20:40 +0100

    Tweak batch normalization.

--- src/graph/layer.ml
+++ src/graph/layer.ml
@@ -1,14 +1,19 @@
+open Core_kernel.Std
 
-let batch_normalization t ~testing ~shape =
+let batch_normalization ?(decay = 0.99) t ~testing ~dims ~feature_count =
   let type_ = Node.output_type t in
   let zero = Ops.const_float ~type_ [ 0. ] in
   let one = Ops.const_float ~type_ [ 1. ] in
-  let beta = Var.create shape ~type_ ~init:zero in
-  let gamma = Var.create shape ~type_ ~init:one in
-  let batch_moments = Ops.moments t ~dims:[ 0 ] in
+  let one_minus_decay = Ops.const_float ~type_ [ 1. -. decay ] in
+  let beta = Var.create [ feature_count ] ~type_ ~init:zero in
+  let gamma = Var.create [ feature_count ] ~type_ ~init:one in
+  let batch_moments = Ops.moments t ~dims:(List.init dims ~f:Fn.id) in
   let beta =
     let update_beta =
-      Ops.assign beta batch_moments.mean
+      (* EWMA update. *)
+      Ops.assignSub
+        beta
+        Ops.(one_minus_decay * (beta - batch_moments.mean))
     in
     Ops.select
       testing
@@ -16,8 +21,11 @@ let batch_normalization t ~testing ~shape =
       (Ops.identity ~control_inputs:[ Node.P update_beta ] beta)
   in
   let gamma =
+      (* EWMA update. *)
     let update_gamma =
-      Ops.assign gamma batch_moments.variance
+      Ops.assignSub
+        gamma
+        Ops.(one_minus_decay * (gamma - batch_moments.variance))
     in
     Ops.select
       testing
@@ -25,4 +33,3 @@ let batch_normalization t ~testing ~shape =
       (Ops.identity ~control_inputs:[ Node.P update_gamma ] gamma)
   in
   Ops.normalize t { mean = beta; variance = gamma }
-
--- src/graph/layer.mli
+++ src/graph/layer.mli
@@ -1,11 +1,16 @@
-(** [batch_normalization node ~testing ~shape] takes as input a node which first dimension is
-    assumed to be the batch dimension. The following dimension have to match [shape].
+(** [batch_normalization ?decay node ~testing ~dims ~feature_count] takes as
+    input a node which last dimension is assumed to be the feature dimension.
+    [dims] has to be the number of dimensions for [node] excluding the feature
+    dimension but including the batch dimension.
+    [feature_count] is the number of features in the last dimension of [node].
     If [testing] is false the batch mean and variance are computed and used to update the
     normalization variables.
 *)
 val batch_normalization
-  : ([< `double | `float ] as 'a) Node.t
+  :  ?decay:float
+  -> ([< `double | `float ] as 'a) Node.t
   -> testing:[ `bool ] Node.t
-  -> shape:int list
+  -> dims:int
+  -> feature_count:int
   -> 'a Node.t
 
