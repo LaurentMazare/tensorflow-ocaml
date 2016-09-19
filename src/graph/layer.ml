5dc298095da191716e3495dedda29a04b87e6c39
laurent <laurent.mazare@gmail.com>
2016-09-19 20:04:38 +0100

    Use ops.cond in the batch normalization layer.

--- src/graph/layer.ml
+++ src/graph/layer.ml
@@ -1,8 +1,6 @@
 open Core_kernel.Std
 
 let batch_normalization ?(decay = 0.99) t ~testing ~dims ~feature_count =
-  (* TODO: use testing in a cond to not modify the beta/gamma vars in this setting. *)
-  ignore testing;
   let type_ = Node.output_type t in
   let zero = Ops.const_float ~type_ (List.init feature_count ~f:(fun _ -> 0.)) in
   let one = Ops.const_float ~type_ (List.init feature_count ~f:(fun _ -> 1.)) in
@@ -10,7 +8,7 @@ let batch_normalization ?(decay = 0.99) t ~testing ~dims ~feature_count =
   let beta = Var.create [ feature_count ] ~type_ ~init:zero in
   let gamma = Var.create [ feature_count ] ~type_ ~init:one in
   let batch_moments = Ops.moments t ~dims:(List.init dims ~f:Fn.id) in
-  let beta =
+  let beta_with_update =
     let update_beta =
       (* EWMA update. *)
       Ops.assignSub
@@ -19,7 +17,7 @@ let batch_normalization ?(decay = 0.99) t ~testing ~dims ~feature_count =
     in
     Ops.identity ~control_inputs:[ Node.P update_beta ] beta
   in
-  let gamma =
+  let gamma_with_update =
       (* EWMA update. *)
     let update_gamma =
       Ops.assignSub
@@ -28,4 +26,6 @@ let batch_normalization ?(decay = 0.99) t ~testing ~dims ~feature_count =
     in
     Ops.identity ~control_inputs:[ Node.P update_gamma ] gamma
   in
+  let beta = Ops.cond testing ~if_true:beta ~if_false:beta_with_update in
+  let gamma = Ops.cond testing ~if_true:gamma ~if_false:gamma_with_update in
   Ops.normalize t { mean = beta; variance = gamma }
