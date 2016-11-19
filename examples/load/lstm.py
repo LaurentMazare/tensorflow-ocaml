505c20516011617636239e8351015bc84f91b755
laurent <laurent.mazare@gmail.com>
2016-11-19 21:36:04 +0000

    Add a lstm example.

---
+++ examples/load/lstm.py
@@ -0,0 +1,24 @@
+import tensorflow as tf
+sess = tf.Session()
+
+from keras import backend as K
+K.set_learning_phase(0)
+K.set_session(sess)
+
+import time
+import numpy as np
+
+import keras.optimizers
+from keras.layers import Embedding, Dense, LSTM, TimeDistributed
+from keras.models import Sequential
+size = 64
+model = Sequential()
+input_shape=(None, 3)
+model.add(LSTM(size, return_sequences=True, consume_less='gpu', input_shape=input_shape))
+model.add(TimeDistributed(Dense(1, activation='sigmoid'), input_shape=(None, size)))
+
+tf.train.write_graph(sess.graph.as_graph_def(), '.', 'lstm.pbtxt', as_text=True)
+tf.train.write_graph(sess.graph.as_graph_def(), '.', 'lstm.pb', as_text=False)
+print model.input
+print model.output
+print [ str(o.name) for o in sess.graph.get_operations() if o.type == 'Assign' ]
