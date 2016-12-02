33b615967bf511288c62cbe3289652d2b638719a
laurent <laurent.mazare@gmail.com>
2016-12-02 21:13:47 +0000

    Better interface for text helper.

--- src/graph/text_helper.ml
+++ src/graph/text_helper.ml
@@ -1,26 +1,31 @@
 open Core_kernel.Std
 
-let read_file filename =
+type t =
+  { content : (int, Bigarray.int8_unsigned_elt, Bigarray.c_layout) Bigarray.Array1.t
+  ; map : int Int.Map.t
+  }
+
+let create filename =
   let file_descr = Unix.openfile filename [ O_RDONLY ] 0 in
-  let array = Bigarray.Array1.map_file file_descr Int8_unsigned C_layout false 0 in
+  let content = Bigarray.Array1.map_file file_descr Int8_unsigned C_layout false 0 in
   let table = Int.Table.create () in
-  for i = 0 to Bigarray.Array1.dim array - 1 do
-    let v = array.{i} in
-    let v =
-      Hashtbl.find_or_add table v
-        ~default:(fun () -> Hashtbl.length table)
-    in
-    array.{i} <- v
+  for i = 0 to Bigarray.Array1.dim content - 1 do
+    let v = content.{i} in
+    if not (Hashtbl.mem table v) then
+      Hashtbl.add_exn table ~key:v ~data:(Hashtbl.length table)
   done;
   Unix.close file_descr;
-  array, Hashtbl.to_alist table |> Int.Map.of_alist_exn
+  { content
+  ; map = Hashtbl.to_alist table |> Int.Map.of_alist_exn
+  }
 
-let read_file_onehot filename kind =
-  let array1, map = read_file filename in
-  let dim = Bigarray.Array1.dim array1 in
-  let onehot = Bigarray.Array2.create kind C_layout dim (Map.length map) in
+let onehot t kind ~pos ~len =
+  let onehot = Bigarray.Array2.create kind C_layout len (Map.length t.map) in
   Bigarray.Array2.fill onehot 0.;
-  for i = 0 to dim - 1 do
-    onehot.{i, array1.{i}} <- 1.;
+  for i = 0 to len - 1 do
+    let index = Map.find_exn t.map t.content.{pos + i} in
+    onehot.{i, index} <- 1.;
   done;
-  onehot, map
+  onehot
+
+let map t = t.map
