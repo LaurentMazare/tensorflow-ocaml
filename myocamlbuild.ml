59313854ddc5bb06c817cc52072d7658fc84f649
laurent <laurent.mazare@gmail.com>
2016-12-29 19:57:13 +0000

    Simplify the build config.

--- myocamlbuild.ml
+++ myocamlbuild.ml
@@ -2,15 +2,19 @@ open Ocamlbuild_plugin
 let () =
   dispatch (function
     | Before_options ->
-      flag ["ocaml"; "link"] (S [ A "-cclib"; A "-ltensorflow"; A "-cclib"; A "-L../lib" ]);
       Options.use_ocamlfind := true
     | After_rules ->
+      flag ["ocamlmklib"; "c"; "use_tensorflow_c"] (S [ A "-ltensorflow"; A "-L../lib" ]);
+      flag ["link"; "ocaml"; "use_tensorflow_c"]
+        (S[A"-ccopt"; A"-L../lib"; A"-cclib"; A"-ltensorflow"]);
       dep ["c"; "compile"] [ "src/wrapper/c_api.h" ];
-      pdep ["link"] "linkdep" (fun param -> [param]);
       rule "cstubs"
         ~prods:["src/wrapper/%_stubs.c"; "src/wrapper/%_generated.ml"]
         ~deps: ["src/wrapper/%_gen.byte"]
         (fun env build ->
           Cmd (A(env "src/wrapper/%_gen.byte")));
-      ocaml_lib "src/tensorflow"
+      ocaml_lib "tensorflow";
+      dep ["link"; "ocaml"; "use_tensorflowcstubs"] ["libtensorflowcstubs.a"];
+      flag ["link"; "ocaml"; "use_tensorflow"]
+        (S[A"-ccopt"; A"-L../lib"; A"-cclib"; A"-ltensorflow"]);
     | _ -> ())
