88420df220b35205655cebf874e1a31b8ce1bdb0
laurent <laurent.mazare@gmail.com>
2016-12-31 12:23:28 +0000

    Simplify build.

--- myocamlbuild.ml
+++ myocamlbuild.ml
@@ -4,9 +4,6 @@ let () =
     | Before_options ->
       Options.use_ocamlfind := true
     | After_rules ->
-      flag ["ocamlmklib"; "c"; "use_tensorflow_c"] (S [ A "-ltensorflow"; A "-L../lib" ]);
-      flag ["link"; "ocaml"; "use_tensorflow_c"]
-        (S[A"-ccopt"; A"-L../lib"; A"-cclib"; A"-ltensorflow"]);
       dep ["c"; "compile"] [ "src/wrapper/c_api.h" ];
       rule "cstubs"
         ~prods:["src/wrapper/%_stubs.c"; "src/wrapper/%_generated.ml"]
