069b23f2d7e5fd1368b4eeabbe75c8c5bbde6aa3
laurent <laurent.mazare@gmail.com>
2017-01-07 10:58:16 +0000

    Use imagemagick to rescale the image to the proper size.

--- examples/fnn/vgg19.ml
+++ examples/fnn/vgg19.ml
@@ -1,10 +1,7 @@
 open Core_kernel.Std
 open Tensorflow
 
-let label_count = Mnist_helper.label_count
-let image_dim = Mnist_helper.image_dim
-let epochs = 2000
-let batch_size = 256
+let img_size = 224
 
 let vgg19 () =
   let block iter ~out_channels x =
@@ -15,9 +12,9 @@ let vgg19 () =
       |> Fnn.relu)
     |> Fnn.max_pool ~filter:(2, 2) ~strides:(2, 2) ~padding:`same
   in
-  let input, input_id = Fnn.input ~shape:(D1 (224*224*3)) in
+  let input, input_id = Fnn.input ~shape:(D1 (img_size*img_size*3)) in
   let model =
-    Fnn.reshape input ~shape:(D3 (224, 224, 3))
+    Fnn.reshape input ~shape:(D3 (img_size, img_size, 3))
     |> block 2 ~out_channels:64
     |> block 2 ~out_channels:128
     |> block 4 ~out_channels:256
@@ -34,19 +31,26 @@ let vgg19 () =
   in
   input_id, model
 
+let load_image ~filename =
+  let image_handle = Magick.read_image ~filename in
+  Magick.Imper.resize image_handle ~width:img_size ~height:img_size ~blur:0.1 ~filter:Cubic;
+  let raw = Magick.Imper.get_raw_without_alpha image_handle in
+  let tensor = Tensor.create3 Float32 img_size img_size 3 in
+  for i = 0 to img_size - 1 do
+    for j = 0 to img_size - 1 do
+      let red, green, blue = raw.(i).(j) in
+      Tensor.set tensor [| i; j; 0 |] (float blue -. 103.939);
+      Tensor.set tensor [| i; j; 1 |] (float green -. 116.779);
+      Tensor.set tensor [| i; j; 2 |] (float red -. 123.68);
+    done;
+  done;
+  tensor
+
 let () =
-  let { Mnist_helper.train_images; train_labels; test_images; test_labels } =
-    Mnist_helper.read_files ()
-  in
+  let input_tensor = load_image ~filename:"input.png" in
   let input_id, model = vgg19 () in
-  Fnn.Model.fit model
-    ~loss:(Fnn.Loss.cross_entropy `sum)
-    ~optimizer:(Fnn.Optimizer.adam ~learning_rate:1e-4 ())
-    ~epochs
-    ~batch_size
-    ~input_id
-    ~xs:train_images
-    ~ys:train_labels;
-  let test_results = Fnn.Model.predict model [ input_id, test_images ] in
-  printf "Accuracy: %.2f%%\n%!" (100. *. Mnist_helper.accuracy test_results test_labels)
+  let results = Fnn.Model.predict model [ input_id, input_tensor ] in
+  for i = 0 to 999 do
+    printf "%d -> %f\n" i (Tensor.get results [| 0; i |] )
+  done
 
