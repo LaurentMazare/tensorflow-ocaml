c84954bb038160a70ddcc3cfb5e9c82dbc75d383
laurent <laurent.mazare@gmail.com>
2017-02-18 12:23:49 +0000

    ocamlbuild fixes.

--- myocamlbuild.ml
+++ myocamlbuild.ml
@@ -12,10 +12,12 @@ let () =
           Cmd (A(env "src/wrapper/%_gen.byte")));
       ocaml_lib "tensorflow";
       ocaml_lib "tensorflow_core";
+      dep ["link"; "ocaml"; "use_tensorflowcstubs"]
+        ["libtensorflowcstubs.a"];
       flag ["link"; "ocaml"; "use_tensorflowcstubs"]
         (S[A"-cclib"; A"-ltensorflowcstubs"; A"-cclib"; A"-ltensorflow"]);
       flag ["link"; "ocaml"; "use_tensorflow"]
-        (S[A"-ccopt"; A"-L../lib"; A"-cclib"; A"-ltensorflow"]);
+        (S[A"-ccopt"; A"-L."; A"-ccopt"; A"-L../lib"; A"-cclib"; A"-ltensorflow"]);
       flag ["link"; "ocaml"; "use_tensorflow_core"]
-        (S[A"-ccopt"; A"-L../lib"; A"-cclib"; A"-ltensorflow"]);
+        (S[A"-ccopt"; A"-L."; A"-ccopt"; A"-L../lib"; A"-cclib"; A"-ltensorflow"]);
     | _ -> ())
