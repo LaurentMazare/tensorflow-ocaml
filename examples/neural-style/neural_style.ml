84c4cee74fe11d776c74abbf3d0dfbc3ea3662fe
laurent <laurent.mazare@gmail.com>
2017-02-27 15:37:21 +0000

    Handle non-square images.

--- examples/neural-style/neural_style.ml
+++ examples/neural-style/neural_style.ml
@@ -1,8 +1,7 @@
 open Core_kernel.Std
 open Tensorflow
 
-let img_size = 224 * 2
-let epochs = 100
+let epochs = 1000
 let learning_rate = 1.
 let cpkt_filename = Sys.getcwd () ^ "/vgg19.cpkt"
 
@@ -32,7 +31,7 @@ let load vars ~filename =
     ~targets:load_and_assign_nodes
     Session.Output.empty
 
-let style_grams_and_content_nodes input =
+let style_grams_and_content_nodes input ~img_h ~img_w =
   let var_by_name = String.Table.create () in
   let block iter ~block_idx ~in_channels ~out_channels (node, acc) =
     List.init iter ~f:Fn.id
@@ -47,7 +46,7 @@ let style_grams_and_content_nodes input =
     |> fun (node, acc_relus) -> max_pool node, List.rev acc_relus :: acc
   in
   let _model, acc_relus =
-    (Ops.reshape input (Ops.const_int ~type_:Int32 [ 1; img_size; img_size; 3 ]), [])
+    (Ops.reshape input (Ops.const_int ~type_:Int32 [ 1; img_h; img_w; 3 ]), [])
     |> block 2 ~block_idx:1 ~in_channels:3   ~out_channels:64
     |> block 2 ~block_idx:2 ~in_channels:64  ~out_channels:128
     |> block 4 ~block_idx:3 ~in_channels:128 ~out_channels:256
@@ -83,23 +82,31 @@ let unnormalize x ~channel =
   min 255 (int_of_float (x +. imagenet_mean channel))
   |> max 0
 
-let load_image ~filename =
+let load_image ~filename ~resize =
   let image = OImages.load filename [] in
   let image = OImages.rgb24 image in
   let width = image # width in
   let height = image # height in
-  let min_edge = min width height in
-  let image =
-    image # sub
-      ((width-min_edge) / 2)
-      ((height-min_edge) / 2)
-      min_edge
-      min_edge
+  let img_w, img_h, image =
+    match resize with
+    | None -> width, height, image
+    | Some (img_h, img_w) ->
+      let ratio = float img_h /. float img_w in
+      let target_height = Float.min (float width *. ratio) (float height) in
+      let target_width = target_height /. ratio |> Float.to_int in
+      let target_height = Float.to_int target_height in
+      let image =
+        image # sub
+          ((width-target_width) / 2)
+          ((height-target_height) / 2)
+          target_width
+          target_height
+      in
+      img_w, img_h, image # resize None img_w img_h
   in
-  let image = image # resize None img_size img_size in
-  let tensor = Tensor.create3 Float32 img_size img_size 3 in
-  for i = 0 to img_size - 1 do
-    for j = 0 to img_size - 1 do
+  let tensor = Tensor.create3 Float32 img_h img_w 3 in
+  for i = 0 to img_h - 1 do
+    for j = 0 to img_w - 1 do
       let { Color.r; g; b } = image # get j i in
       Tensor.set tensor [| i; j; 0 |] (normalize b ~channel:`blue);
       Tensor.set tensor [| i; j; 1 |] (normalize g ~channel:`green);
@@ -108,13 +115,13 @@ let load_image ~filename =
   done;
   tensor
 
-let save_image tensor ~filename =
+let save_image tensor ~filename ~img_h ~img_w =
   let total_size = Array.fold (Tensor.dims tensor) ~init:1 ~f:( * ) in
-  if total_size <> img_size * img_size * 3
+  if total_size <> img_h * img_w * 3
   then failwith "Incorrect tensor size";
-  let image = new OImages.rgb24 img_size img_size in
-  for i = 0 to img_size - 1 do
-    for j = 0 to img_size - 1 do
+  let image = new OImages.rgb24 img_w img_h in
+  for i = 0 to img_h - 1 do
+    for j = 0 to img_w - 1 do
       let b = Tensor.get tensor [| i; j; 0 |] |> unnormalize ~channel:`blue in
       let g = Tensor.get tensor [| i; j; 1 |] |> unnormalize ~channel:`green in
       let r = Tensor.get tensor [| i; j; 2 |] |> unnormalize ~channel:`red in
@@ -123,40 +130,39 @@ let save_image tensor ~filename =
   done;
   image # save filename None []
 
-let compute_grams ~filename =
-  let input_placeholder = Ops.placeholder ~type_:Float [ img_size; img_size; 3 ] in
+let compute_grams ~filename ~img_h ~img_w =
+  let input_placeholder = Ops.placeholder ~type_:Float [ img_h; img_w; 3 ] in
   let style_grams, _ =
     style_grams_and_content_nodes (Ops.Placeholder.to_node input_placeholder)
+      ~img_h ~img_w
   in
-  let input_tensor = load_image ~filename in
-  save_image input_tensor ~filename:"cropped.png";
+  let input_tensor = load_image ~filename ~resize:(Some (img_h, img_w)) in
+  save_image input_tensor ~filename:"cropped.png" ~img_h ~img_w;
   List.map style_grams ~f:(fun node ->
     Session.run
       ~inputs:[ Session.Input.float input_placeholder input_tensor ]
       ~targets:[ Node.P node ]
       (Session.Output.float node))
 
-let total_variation_loss input =
+let total_variation_loss input ~img_h ~img_w =
   let input =
-    Ops.reshape input (Ops.const_int ~type_:Int32 [ img_size; img_size; 3 ])
+    Ops.reshape input (Ops.const_int ~type_:Int32 [ img_h; img_w; 3 ])
   in
   let axis1_diff =
     Ops.(-)
-      (Ops.slice input (Ops.ci32 [ 0; 0; 0 ]) (Ops.ci32 [ img_size-1; img_size; 3 ]))
-      (Ops.slice input (Ops.ci32 [ 1; 0; 0 ]) (Ops.ci32 [ img_size-1; img_size; 3 ]))
+      (Ops.slice input (Ops.ci32 [ 0; 0; 0 ]) (Ops.ci32 [ img_h-1; img_w; 3 ]))
+      (Ops.slice input (Ops.ci32 [ 1; 0; 0 ]) (Ops.ci32 [ img_h-1; img_w; 3 ]))
   in
   let axis2_diff =
     Ops.(-)
-      (Ops.slice input (Ops.ci32 [ 0; 0; 0 ]) (Ops.ci32 [ img_size; img_size-1; 3 ]))
-      (Ops.slice input (Ops.ci32 [ 0; 1; 0 ]) (Ops.ci32 [ img_size; img_size-1; 3 ]))
+      (Ops.slice input (Ops.ci32 [ 0; 0; 0 ]) (Ops.ci32 [ img_h; img_w-1; 3 ]))
+      (Ops.slice input (Ops.ci32 [ 0; 1; 0 ]) (Ops.ci32 [ img_h; img_w-1; 3 ]))
   in
-  Ops.(/)
-    Ops.(reduce_sum (axis1_diff * axis1_diff) + reduce_sum (axis2_diff * axis2_diff))
-    (Ops.f (float (img_size * img_size)))
+  Ops.(reduce_sum (axis1_diff * axis1_diff) + reduce_sum (axis2_diff * axis2_diff))
 
-let create_and_set_var tensor =
-  let input_var = Var.f_or_d [ img_size; img_size; 3 ] ~type_:Float 0. in
-  let placeholder = Ops.placeholder [ img_size; img_size; 3 ] ~type_:Float in
+let create_and_set_var tensor ~img_h ~img_w =
+  let input_var = Var.f_or_d [ img_h; img_w; 3 ] ~type_:Float 0. in
+  let placeholder = Ops.placeholder [ img_h; img_w; 3 ] ~type_:Float in
   let assign = Ops.assign input_var (Ops.Placeholder.to_node placeholder) in
   Session.run
     ~inputs:[ Session.Input.float placeholder tensor ]
@@ -165,12 +171,19 @@ let create_and_set_var tensor =
   input_var
 
 let () =
+  let input_tensor = load_image ~filename:"input.png" ~resize:None in
+  let img_h, img_w =
+    match Tensor.dims input_tensor with
+    | [| img_h; img_w; _ |] -> img_h, img_w
+    | _ -> assert false
+  in
   printf "Computing target features...\n%!";
-  let target_grams = compute_grams ~filename:"style.png" in
+  let target_grams = compute_grams ~filename:"style.png" ~img_h ~img_w in
   printf "Done computing target features...\n%!";
-  let input_tensor = load_image ~filename:"input.png" in
-  let input_var = create_and_set_var input_tensor in
-  let style_grams, content_nodes = style_grams_and_content_nodes input_var in
+  let input_var = create_and_set_var input_tensor ~img_h ~img_w in
+  let style_grams, content_nodes =
+    style_grams_and_content_nodes input_var ~img_h ~img_w
+  in
   let style_losses, style_inputs =
     List.map2_exn style_grams target_grams ~f:(fun gram_node target_gram ->
       let dims = Tensor.dims target_gram |> Array.to_list in
@@ -195,7 +208,7 @@ let () =
   let loss =
     Ops.(List.reduce_exn style_losses ~f:(+)
       + List.reduce_exn content_losses ~f:(+)
-      + total_variation_loss input_var)
+      + total_variation_loss input_var ~img_h ~img_w)
   in
   let gd =
     Optimizers.adam_minimizer loss
@@ -210,5 +223,5 @@ let () =
         Session.Output.(both (float input_var) (scalar_float loss))
     in
     printf "epoch: %d   loss: %f\n%!" epoch loss;
-    save_image output_tensor ~filename:(sprintf "out_%d.png" epoch);
+    save_image output_tensor ~filename:(sprintf "out_%d.png" epoch) ~img_h ~img_w;
   done
