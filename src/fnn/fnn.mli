267a18bb6ce0a09e70e13fb0b6a41c3a2102588f
Anders Fugmann <anders@fugmann.net>
2019-04-01 16:18:41 +0200

    Change epoch to mean total iterations over all samples, rather than steps

--- src/fnn/fnn.mli
+++ src/fnn/fnn.mli
@@ -143,6 +143,12 @@ module Model : sig
     -> (Input_id.t * (float, 'c) Tensor.t) list
     -> (float, 'c) Tensor.t
 
+  (** [fit ~xs ~ys ?batch_size ~epoch ...] trains a model on the [xs] and [ys] as training data.
+      Training will be done in batches of size [batch_size]. The total amount of training steps
+      is {!epochs * |xs| / batch_size}. The average err is printed for each epoch ({!|xs| / bach_size}) steps.
+      if [batch_size] is not given or is larger than {!|xs|}, it defaults to {!|xs|}.
+      After each epoch (|xs| / batch_size) the average loss for the epoch is printed to {!stdout}.
+  *)
   val fit
     :  ?addn_inputs:(Input_id.t * (float, 'c) Tensor.t) list
     -> ?batch_size:int
